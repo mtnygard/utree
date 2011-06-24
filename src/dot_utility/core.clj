89d69fd1634b6e58f5b6a4887b9ef7bc1cedfff7
Michael Nygard <mtnygard@gmail.com>
2011-06-24 15:39:53 -0500

    clean up code a bit

--- src/dot_utility/core.clj
+++ src/dot_utility/core.clj
@@ -2,10 +2,8 @@
   (:require (clojure [string :as str]))
   (:gen-class))
 
-(def id-sequence (atom 0))
-(defn get-id! [] (str "node" (dosync (swap! id-sequence inc))))
-
-(defn make-node [t l] {:id (get-id!) :level t :label l})
+(let [id-sequence (atom 0)]
+  (defn make-node [t l] {:id (str "node" (dosync (swap! id-sequence inc))) :level t :label l}))
 
 (defn add-node [g n] (if (g n) g (assoc g n {:next #{} :prev #{}})))
 
@@ -16,20 +14,16 @@
      (update-in [n1 :next] conj n2)
      (update-in [n2 :prev] conj n1)))
 
-(defn contains-node? [g n] (g n))
-(defn contains-edge? [g n1 n2] (get-in g [n1 :next n2]))
 (defn next-nodes [g n] (get-in g [n :next]))
 (defn prev-nodes [g n] (get-in g [n :prev]))
 (defn nodes [g] (keys g))
 
-(defn split-lines [str]
-  (filter (comp not empty?) (str/split str #"\n")))
+(defn split-lines [str] (filter (comp not empty?) (str/split str #"\n")))
 
 (defn nodify
   [line]
-  (let [stars (count (take-while #(= \* %) line))
-        label (clojure.string/trim (subs line stars))]
-    (if (> stars 0) (make-node stars label))))
+  (let [stars (count (take-while #(= \* %) line))]
+    (if (> stars 0) (make-node stars (clojure.string/trim (subs line stars))))))
 
 (defn insert-root [nodes] (conj nodes (make-node 0 "Utility")))
 
@@ -43,39 +37,28 @@
           {}
           (partition 2 1 nodeseq)))
 
-(defn prolog
-  [g]
-  (println "digraph {
-     rankdir=LR;
-     node[shape=\"plaintext\"];
-     edge[arrowhead=\"none\"];"))
-
-(defn rank
-  [nodes]
+(defn emit-rank
+  [[r nodes]]
   (println "subgraph {\nrank=same;")
   (doseq [n nodes]
     (println (str (:id n) " [label=\"" (:label n) "\"];")))
   (println "}"))
 
-(defn dependencies
+(defn emit-dependencies
   [g]
   (doseq [n (nodes g)
-        trg (next-nodes g n)]
+          trg (next-nodes g n)]
     (println (str (:id n) "->" (:id trg) ";"))))
-
-(defn epilog
-  [g]
-  (println "}"))
-
-(defn ranks-in [g] (set (map :level (nodes g))))
 
 (defn emit-dot
   [g]
-  (prolog g)
-  (doseq [r (ranks-in g)]
-    (rank (filter #(= r (:level %)) (nodes g))))
-  (dependencies g)
-  (epilog g))
+  (println "digraph {
+     rankdir=LR;
+     node[shape=\"plaintext\"];
+     edge[arrowhead=\"none\"];")
+  (doall (map emit-rank (sort (group-by :level (keys g)))))
+  (emit-dependencies g)
+  (println "}"))
 
 (defn -main [& args]
   (doseq [f args]
