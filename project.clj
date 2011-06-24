e4e8f1d6463cfa1b3a454b689c8cab18427e4384
Michael Nygard <mtnygard@gmail.com>
2011-06-24 15:00:51 -0500

    Alpha release number.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject dot-utility
-  :version :git
+(defproject dot-utility "0.0.1"
   :description "Create a utility tree from simple text format"
   :dependencies [[clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
