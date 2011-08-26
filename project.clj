09eec4277202ac66ebb8d6d39d4c297e59b13283
Michael Nygard <mtnygard@gmail.com>
2011-08-26 13:21:22 -0500

    Checkpoint: Clojurescript compilation at least does _something_

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject utree "0.0.2"
   :description "Create a utility tree from simple text format"
-  :dependencies [[clojure "1.2.1"]
+  :dependencies [[clojure "1.3.0-beta1"]
                  [clojure-contrib "1.2.0"]
                  [org.clojure/data.json "0.1.1"]
                  [fleet "0.9.4"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-                     [midje "1.2-alpha4"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :main utree.core)
