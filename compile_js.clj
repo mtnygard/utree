3c6c049005da4b9dd8c489c282929661ebfd282e
Michael Nygard <mtnygard@gmail.com>
2012-04-20 08:31:42 -0500

    Remove thoroughly outdate ClojureScript attempt

--- compile_js.clj
+++
@@ -1,8 +0,0 @@
-(require '[cljs.closure :as closure])
-
-(if (= 1 (count *command-line-args*))
-  (closure/build (first *command-line-args*)
-                 {:optimizations :simple
-                  :output-dir "templates/out"
-                  :output-to "templates/main.js"})
-  (println "compile_js.clj requires exactly one argument, the path to the cljs file to compile"))
